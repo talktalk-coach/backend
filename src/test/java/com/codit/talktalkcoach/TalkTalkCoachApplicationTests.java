86e78905eff6479673be7807129160e8d19bd4ea
yunji1014 <7787yunji@gmail.com>
2026-03-31 15:15:50 +0900

    setting

---
+++ src/test/java/com/codit/talktalkcoach/TalkTalkCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codit.talktalkcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTalkCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
